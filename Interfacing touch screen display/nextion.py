dc37e55e6eb541c6bd193fbf12a3debaa5cf08de
Ramandeep Kaur <74325887+Davinderaman@users.noreply.github.com>
2021-03-05 10:01:24 -0500

    Update nextion.py

--- Interfacing touch screen display/nextion.py
+++ Interfacing touch screen display/nextion.py
@@ -1,1 +1,6 @@
-
+import Adafruit_BBIO.UART as UART
+        UART.setup("UART1")
+        Import serial 
+serialobj= serial.Serial(port = "/dev/ttyO1",  baudrate=9600, timeout=0.3)
+While(1):
+print(serialobj.read(10))
