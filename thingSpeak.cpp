652e5712d474c582a3af5855fc07bee0a7dc553f
Amit kumar <55963691+amitkumar-sudo@users.noreply.github.com>
2021-03-15 13:33:16 -0400

    Update thingSpeak.cpp

--- thingSpeak.cpp
+++ thingSpeak.cpp
@@ -11,17 +11,17 @@ using namespace internetOfThings;
 void thingSpeak(int sendData){
    ostringstream head, data;
    cout << "Starting ThingSpeak Example" << endl;
-   SocketClient sc("api.thingspeak.com",80);
-   data << "field1=" << sendData << endl;
-   cout << "Check : "<<sc.connectToServer();
-   head << "POST /update HTTP/1.1\n"
-        << "Host: api.thingspeak.com\n"
+   SocketClient sc("api.thingspeak.com",80);   // socketClient header file access through this function
+   data << "field1=" << sendData << endl;  // sending data to thre field 1 on the channel
+   cout << "Check : "<<sc.connectToServer();  //setting connection with server
+   head << "POST /update HTTP/1.1\n"  // adding the http request from bbb wireless
+        << "Host: api.thingspeak.com\n"  // host adress 
         << "Connection: close\n"
-        << "X-THINGSPEAKAPIKEY: L41OHUV7IBZ01UWC\n"
-        << "Content-Type: application/x-www-form-urlencoded\n"
-        << "Content-Length:" << string(data.str()).length() << "\n\n";
+        << "X-THINGSPEAKAPIKEY: L41OHUV7IBZ01UWC\n"  //write api key for the channel on thingSpeak
+        << "Content-Type: application/x-www-form-urlencoded\n"  // describing the content type 
+        << "Content-Length:" << string(data.str()).length() << "\n\n";  // calculating the string length 
    sc.send(string(head.str()));
-   sc.send(string(data.str()));
+   sc.send(string(data.str()));  // snding the total amount to thingspeak
    string rec = sc.receive(1024);
    cout << "[" << rec << "]" << endl;
    cout << "End of ThingSpeak Example" << endl;
@@ -29,7 +29,7 @@ void thingSpeak(int sendData){
 
 
 int main() {
-   int calculated_amount=850;
-   thingSpeak(calculated_amount);
+   int calculated_amount=850;  // adding the amount to a varible
+   thingSpeak(calculated_amount);  // calling the thingSpeak function
    return 0;
 }
