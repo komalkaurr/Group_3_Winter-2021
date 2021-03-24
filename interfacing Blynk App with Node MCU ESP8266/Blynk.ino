72c10dfd4a0d4f299f58a5307ca828a13ba6872e
Komal Kaur <32121359+komalkaurr@users.noreply.github.com>
2021-03-24 09:05:50 -0400

    Update Blynk.ino

--- interfacing Blynk App with Node MCU ESP8266/Blynk.ino
+++ interfacing Blynk App with Node MCU ESP8266/Blynk.ino
@@ -1,9 +1,12 @@
 
-#define BLYNK_PRINT Serial
+#define BLYNK_PRINT Serial   //The compiler will replace any mention of BLYNK_PRINT with the Serial at compile time. 
 
 
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+
+#include <ESP8266WiFi.h>       //This library provides ESP8266 specific Wi-Fi routines that we are calling to connect to the network.
+
+#include <BlynkSimpleEsp8266.h>           //This library is associated with  Blynk application
+
 
 String recieve; //global string declaration for recieveing data
 
@@ -26,8 +29,11 @@ void setup()
 
 void loop()
 {
-  Blynk.notify("There is a gas leakege in restaurant");
-  Blynk.email("komaldeora024@gmail.com" , "Alert from Sensor " ,"There is gas leakage in restaurant");
-  Blynk.run();
+  Blynk.notify("There is a gas leakege in restaurant");           //This function is associated with notification widget. We can print any string by passing it in its argument.
+
+  Blynk.email("komaldeora024@gmail.com" , "Alert from Sensor " ,"There is gas leakage in restaurant");   //Thisfunction is associated with email notification.
+
+  Blynk.run();                   //Blynk.run() is a main Blynk routine responsible for keeping connection alive, sending data, receiving data, etc.
+
   delay(30000);    // delay of 30s
 }
